fcf0a77718d44b9bd5216ec1f883f7548f902b06
tuxedo0801 <alex@root1.de>
2016-02-10 16:09:21 +0100

    fixed 24hrs display

--- src/main/java/de/konnekting/suite/uicomponents/groupmonitor/GroupAddressEventContainer.java
+++ src/main/java/de/konnekting/suite/uicomponents/groupmonitor/GroupAddressEventContainer.java
@@ -17,7 +17,7 @@ import java.util.Date;
  */
 public class GroupAddressEventContainer {
     
-    private static final SimpleDateFormat sdf = new SimpleDateFormat("YYYY-MM-dd hh:mm:ss.SSS");
+    private static final SimpleDateFormat sdf = new SimpleDateFormat("YYYY-MM-dd HH:mm:ss.SSS");
     
     private final int number;
     private final Date timestamp;
